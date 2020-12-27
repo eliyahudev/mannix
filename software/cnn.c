eaec39b9b7caaa0762683c9cbed26d885ef16ad7
eliyahudev <eliyahulevyy@gmail,com>
2020-12-27 19:59:14 +0200

    fix mul function

--- software/cnn.c
+++ software/cnn.c
@@ -48,18 +48,24 @@ void deleteMatrix(Matrix* m1) {
             free(m1->data[i]);
     }
     free(m1->data);
-    free(m1);   
+    free(m1);
     printf ("memory is free\n");
 }
 
-void setMatrixValues(Matrix* m1, char* path) {
-    int *x = getData(path, m1->rows*m1->cols);
+void setMatrixValues(Matrix* m1, FILE* filePointer, int* label) {
+    int *x = getData(filePointer, m1->rows*m1->cols + 1);
+    int k = 0;
+    *label = x[k++];
+    for (size_t i = 0; i < m1->rows; i++) {
+        for (size_t j = 0; j < m1->cols; j++) {
+            m1->data[i][j] = x[k++];
+        }
+    } 
     free(x);
-    printf("done\n");
+    //printf("done\n");
     return;
-
 }
-/*
+
 void addMatrix(Matrix* m1, Matrix* m2) {
     if (m1->rows != m2->rows || m1->cols != m2->cols) {
         printf("DImension ERRER - Matricies sizes are not equal");
@@ -84,22 +90,21 @@ void subMatrix(Matrix* m1, Matrix* m2) {
     }
 }
 
-void mulMatrix(Matrix* m1, Matrix* m2) {
+void mulMatrix(Matrix* m1, Matrix* m2, Matrix* m3) {
     if (m1->cols != m2->rows) {
         printf("DImension ERRER - Matricies sizes are not equal");
         return;
     }
-    Matrix* m3 = creatMatrix(m1->rows, m2->cols);
+
     for (int i=0; i<m1->rows; i++) {
         for (int j=0; j<m2->cols; j++) {
-                for (int k=0; k<m1->cols; k++) 
+                for (int k=0; k<m1->cols; k++) {
                     m3->data[i][j] += m1->data[i][k] * m2->data[k][j]; 
+                }
         }
     }
-    deleteMatrix(m1);
-    m1 = m3;
 }
-
+/*
 void asignMatrix(Matrix* m1, Matrix* m2) {
     if (m1->rows != m2->rows || m1->cols != m2->cols) {
         printf("DImension ERRER - Matricies sizes are not equal");
@@ -120,11 +125,33 @@ int main() {
 
     Matrix* m2 = creatMatrix(4,2);
     Matrix* m1 = creatMatrix(2,4);
+    Matrix* m3 = creatMatrix(2,2);;
+    int m1_label, m2_label;
+    FILE *filePointer;
+
+    filePointer = fopen("r.csv", "r");
   //  mulMatrix(m2,m1);
+
     printMatrix(m2);
     printMatrix(m1);
-    setMatrixValues(m1, "test.csv");
+
+    setMatrixValues(m1, filePointer, &m1_label);
+    setMatrixValues(m2, filePointer, &m2_label);
+    
+    //mulMatrix(m1,m2);
+    
+    // printf("label = %d\n", m1_label);
+    // printMatrix(m1);
+    // printf("label = %d\n", m2_label);
+    // printMatrix(m2);
+
+    mulMatrix(m1, m2, m3);
+    printMatrix(m3);
+
     deleteMatrix(m1);
     deleteMatrix(m2);
+    deleteMatrix(m3);
+    fclose(filePointer);
+
     return 0;
 }
