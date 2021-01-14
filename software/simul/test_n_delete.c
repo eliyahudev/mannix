6be8c915f188d7928e2c64d350640db97715ed12
eliyahudev <eliyahulevyy@gmail.com>
2021-01-15 01:57:46 +0200

    and matrix functions and fix memory allocation

---
+++ software/simul/test_n_delete.c
@@ -0,0 +1,22 @@
+/* Change console text color, then restore it back to normal. */
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    WORD saved_attributes;
+
+    /* Save current attributes */
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+    saved_attributes = consoleInfo.wAttributes;
+
+    SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+    printf("This is some nice COLORFUL text, isn't it?");
+
+    /* Restore original attributes */
+    SetConsoleTextAttribute(hConsole, saved_attributes);
+    printf("Back to normal");
+
+    return 0;
+}
